eacf5a64a2f0ebae40a6711d7ab20accc8ad31e9
Justin James <jjames@renci.org>
2021-06-01 18:31:44 +0000

    [1968] added eu-central-1 tests for minio

--- irods_consortium_continuous_integration_test_hook.py
+++ irods_consortium_continuous_integration_test_hook.py
@@ -61,7 +61,12 @@ def download_and_start_minio_server():
     os.environ['MINIO_ACCESS_KEY'] = access_key
     os.environ['MINIO_SECRET_KEY'] = secret_key
    
-    return subprocess.Popen(['./minio', 'server', '/data'])
+    proc1 = subprocess.Popen(['./minio', 'server', '/data'])
+
+    os.environ['MINIO_REGION_NAME'] = 'eu-central-1' 
+    proc2 = subprocess.Popen(['./minio', 'server', '--address', ':9001', '/data2'])
+
+    return (proc1, proc2)
 
 
 def main():
@@ -78,14 +83,15 @@ def main():
     irods_python_ci_utilities.install_os_packages_from_files(glob.glob(os.path.join(os_specific_directory, 'irods-resource-plugin-s3*.{0}'.format(package_suffix))))
 
     install_build_prerequisites()
-    minio_process = download_and_start_minio_server()
+    minio_processes = download_and_start_minio_server()
 
     time.sleep(10)
 
     try:
         test_output_file = 'log/test_output.log'
         irods_python_ci_utilities.subprocess_get_output(['sudo', 'su', '-', 'irods', '-c', 'python2 scripts/run_tests.py --xml_output --run_s test_irods_resource_plugin_s3_minio 2>&1 | tee {0}; exit $PIPESTATUS'.format(test_output_file)], check_rc=True)
-        minio_process.terminate()
+        minio_processes[0].terminate()
+        minio_processes[1].terminate()
     finally:
         if output_root_directory:
             irods_python_ci_utilities.gather_files_satisfying_predicate('/var/lib/irods/log', output_root_directory, lambda x: True)
