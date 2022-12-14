af30d1416cd0451be114d2b2ce285c679d7fce6b
Justin James <jjames@renci.org>
2022-12-14 11:57:20 -0500

    added wasabi tests

---
+++ packaging/test_irods_resource_plugin_s3_wasabi.py
@@ -0,0 +1,128 @@
+
+import datetime
+import os
+import platform
+import random
+import re
+import shutil
+import string
+import subprocess
+import urllib3
+
+from .resource_suite_s3_nocache import Test_S3_NoCache_Base
+from .resource_suite_s3_nocache import Test_S3_NoCache_Glacier_Base
+from .resource_suite_s3_cache import Test_S3_Cache_Base
+from .resource_suite_s3_cache import Test_S3_Cache_Glacier_Base
+
+import sys
+if sys.version_info >= (2,7):
+    import unittest
+else:
+    import unittest2 as unittest
+
+from .. import lib
+from . import session
+from ..configuration import IrodsConfig
+from .resource_suite import ResourceSuite
+from .test_chunkydevtest import ChunkyDevTest
+
+
+class Test_Compound_With_S3_Resource(Test_S3_Cache_Base, unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        """Set up the test."""
+        self.keypairfile='/var/lib/irods/wasabi.keypair'
+        self.archive_naming_policy='decoupled'
+        self.s3stsdate=''
+        self.s3region='us-east-1'
+        self.s3endPoint='s3.wasabisys.com'
+        self.s3sse = 0 # server side encryption
+        super(Test_Compound_With_S3_Resource, self).__init__(*args, **kwargs)
+
+class Test_Compound_With_S3_Resource_V4_SSE(Test_S3_Cache_Base, unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        """Set up the test."""
+        self.keypairfile='/var/lib/irods/wasabi.keypair'
+        self.archive_naming_policy='decoupled'
+        self.s3stsdate=''
+        self.s3sse=1
+        self.s3region='us-east-1'
+        self.s3endPoint='s3.wasabisys.com'
+        super(Test_Compound_With_S3_Resource_V4_SSE, self).__init__(*args, **kwargs)
+
+class Test_S3_NoCache_V4(Test_S3_NoCache_Base, unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        """Set up the test."""
+        self.keypairfile='/var/lib/irods/wasabi.keypair'
+        self.s3region='us-east-1'
+        self.s3endPoint='s3.wasabisys.com'
+        self.s3EnableMPU=1
+        super(Test_S3_NoCache_V4, self).__init__(*args, **kwargs)
+
+class Test_S3_NoCache_MPU_Disabled(Test_S3_NoCache_Base, unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        """Set up the test."""
+        self.keypairfile='/var/lib/irods/wasabi.keypair'
+        self.s3region='us-east-1'
+        self.s3endPoint='s3.wasabisys.com'
+        self.s3EnableMPU=0
+        super(Test_S3_NoCache_MPU_Disabled, self).__init__(*args, **kwargs)
+
+class Test_S3_NoCache_SSE(Test_S3_NoCache_Base, unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        """Set up the test."""
+        self.keypairfile='/var/lib/irods/wasabi.keypair'
+        self.s3region='us-east-1'
+        self.s3endPoint='s3.wasabisys.com'
+        self.s3sse=1
+        self.s3EnableMPU=1
+        super(Test_S3_NoCache_SSE, self).__init__(*args, **kwargs)
+
+class Test_S3_NoCache_Decoupled(Test_S3_NoCache_Base, unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        """Set up the test."""
+        self.keypairfile='/var/lib/irods/wasabi.keypair'
+        self.s3region='us-east-1'
+        self.s3endPoint='s3.wasabisys.com'
+        self.s3EnableMPU=0
+        self.archive_naming_policy = 'decoupled'
+        super(Test_S3_NoCache_Decoupled, self).__init__(*args, **kwargs)
+
+    @unittest.skipIf(True, 'test does not work in decoupled because we are using same bucket for multiple resources')
+    def test_iget_with_stale_replica(self):  # formerly known as 'dirty'
+        pass
+
+    @unittest.skipIf(True, 'test does not work in decoupled because we are using same bucket for multiple resources')
+    def test_irepl_with_purgec(self):
+        pass
+
+    @unittest.skipIf(True, 'test does not work in decoupled because we are using same bucket for multiple resources')
+    def test_put_get_small_file_in_repl_node(self):
+        pass
+
+    @unittest.skipIf(True, 'test does not work in decoupled because we are using same bucket for multiple resources')
+    def test_put_get_large_file_in_repl_node(self):
+        pass
+
+
+#class Test_S3_NoCache_Glacier(Test_S3_NoCache_Glacier_Base, unittest.TestCase):
+#
+#    def __init__(self, *args, **kwargs):
+#        """Set up the test."""
+#        self.keypairfile='/projects/irods/vsphere-testing/externals/amazon_web_services-CI.keypair'
+#        self.s3region='us-east-1'
+#        self.s3endPoint='s3.wasabisys.com'
+#        self.s3EnableMPU=1
+#        super(Test_S3_NoCache_Glacier, self).__init__(*args, **kwargs)
+#
+#class Test_S3_Cache_Glacier(Test_S3_Cache_Glacier_Base, unittest.TestCase):
+#
+#    def __init__(self, *args, **kwargs):
+#        """Set up the test."""
+#        self.keypairfile='/projects/irods/vsphere-testing/externals/amazon_web_services-CI.keypair'
+#        self.s3region='us-east-1'
+#        self.s3endPoint='s3.wasabisys.com'
+#        self.s3EnableMPU=1
+#        self.s3stsdate=''
+#        super(Test_S3_Cache_Glacier, self).__init__(*args, **kwargs)
+
